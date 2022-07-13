2b71000cb6964479f37eaceddf1649570500d947
Loyce Ugwukozor <104769520+Loycemary@users.noreply.github.com>
2022-07-13 01:16:56 +0100

    Update main.h

--- main.h
+++ main.h
@@ -23,4 +23,19 @@ int print_d(va_list args);
 int print_i(va_list args);
 int (*prntfunc(char x))(va_list);
 int print_b(va_list args);
+int print_u(va_list args);
+int print_o(va_list args);
+int print_p(va_list args);
+int print_x(va_list args);
+int print_X(va_list args);
+int print_reversed(va_list args);
+int rot13(va_list args);
+int (*get_func(char x))(va_list);
+
+unsigned int base_len(unsigned int, int);
+char *rev_string(char *);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int print_unsigned_number(unsigned int n);
+void write_base(char *str);
+
 #endif
